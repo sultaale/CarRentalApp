af96ef01d95543b71f5c2504e7f369efa848f4af
hammer2000 <hammer2k2@gmail.com>
2023-09-17 20:54:01 +0300

    corrections

--- src/main/java/com/perz/carrentalapp/model/Order.java
+++ src/main/java/com/perz/carrentalapp/model/Order.java
@@ -1,6 +1,14 @@
 package com.perz.carrentalapp.model;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -48,5 +56,5 @@ public class Order {
     @Column(name ="date_of_change")
     private LocalDate dateChange;
 
-    private long amount;
+    private Long amount;
 }
--- src/main/java/com/perz/carrentalapp/model/TransactionCompany.java
+++ src/main/java/com/perz/carrentalapp/model/TransactionCompany.java
@@ -1,6 +1,15 @@
 package com.perz.carrentalapp.model;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +29,10 @@ public class TransactionCompany {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 
     @Column(name ="transaction_date")
     private LocalDate date;
