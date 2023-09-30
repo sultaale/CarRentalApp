4ffa8f3f9479b36845cdaab55378d72bc4f8f7bd
Alexandra Tyurmina <94904090+sultaale@users.noreply.github.com>
2023-09-30 15:33:05 +0300

    Update OrderRepository.java

--- src/main/java/com/perz/carrentalapp/repositories/OrderRepository.java
+++ src/main/java/com/perz/carrentalapp/repositories/OrderRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.List;
 
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("SELECT o FROM  Order o INNER JOIN o.status s " +
             "WHERE o.car.id = :carId AND (o.end >= :start AND o.start <= :end) " +
