711ce0f98014ed0b2b67b98b9058a2433da881be
Alexandra Tyurmina <94904090+sultaale@users.noreply.github.com>
2023-09-30 15:36:09 +0300

    Update BrandRepository.java
    Code style corrections

--- src/main/java/com/perz/carrentalapp/repositories/BrandRepository.java
+++ src/main/java/com/perz/carrentalapp/repositories/BrandRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
     Optional<Brand> findByName(String name);
 
