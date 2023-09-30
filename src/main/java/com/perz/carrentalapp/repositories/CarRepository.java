f03392ab08603de90f9adc84e90088eab0b840ae
Alexandra Tyurmina <94904090+sultaale@users.noreply.github.com>
2023-09-30 15:35:05 +0300

    Update CarRepository.java

--- src/main/java/com/perz/carrentalapp/repositories/CarRepository.java
+++ src/main/java/com/perz/carrentalapp/repositories/CarRepository.java
@@ -3,6 +3,6 @@ package com.perz.carrentalapp.repositories;
 import com.perz.carrentalapp.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarRepository extends JpaRepository<Car, Long> {
 
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
